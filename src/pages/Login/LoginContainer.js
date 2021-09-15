2dc294d48163a13311ab6e84584643ddb18ba2d8
Leandro Liduvino <leandro.augusto460@gmail.com>
2021-09-15 14:21:26 -0300

    ultimas estilizacoes

---
+++ src/pages/Login/LoginContainer.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export default styled.div`
+/* display: flex; */
+/* justify-content: center; */
+/* align-items: center; */
+/* flex-direction: column; */
+align-self: center;
+margin: auto;
+
+position: relative;
+width: 100vw;
+height: 100vh;
+`;
